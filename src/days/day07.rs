272fb65137f72c8042bf041ca072692f4cdf82b7
Valentin Bersier <vbersier@gmail.com>
2022-11-29 09:53:32 +0100

    style: fix tab spacing

--- src/days/day07.rs
+++ src/days/day07.rs
@@ -1,25 +1,29 @@
-use nom::IResult;
+use nom::{
+    character::complete::{char, u8},
+    multi::separated_list0,
+    IResult,
+};
 
 use crate::days::Day;
 
 pub struct Day07;
 
 impl Day for Day07 {
-  type Input = String;
+    type Input = Vec<u8>;
 
-  fn parse(_input: &str) -> IResult<&str, Self::Input> {
-    unimplemented!("parser")
-  }
+    fn parse(input: &str) -> IResult<&str, Self::Input> {
+        separated_list0(char(','), u8)(input)
+    }
 
-  type Output1 = String;
+    type Output1 = u8;
 
-  fn part_1(_input: &Self::Input) -> Self::Output1 {
-    unimplemented!("part_1")
-  }
+    fn part_1(_input: &Self::Input) -> Self::Output1 {
+        unimplemented!("part_1")
+    }
 
-  type Output2 = String;
+    type Output2 = u8;
 
-  fn part_2(_input: &Self::Input) -> Self::Output2 {
-    unimplemented!("part_2")
-  }
+    fn part_2(_input: &Self::Input) -> Self::Output2 {
+        unimplemented!("part_2")
+    }
 }
