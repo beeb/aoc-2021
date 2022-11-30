9fd7991bec2e109006889717f8ae63b86070736b
Valentin Bersier <vbersier@gmail.com>
2022-11-30 07:42:37 +0100

    feat: day 8 parsing

--- src/days/day08.rs
+++ src/days/day08.rs
@@ -1,20 +1,56 @@
-use nom::IResult;
+use itertools::Itertools;
+use nom::{
+    bytes::complete::tag,
+    character::complete::{alpha1, newline, space1},
+    combinator::map,
+    multi::separated_list0,
+    sequence::tuple,
+    IResult,
+};
 
 use crate::days::Day;
 
 pub struct Day08;
 
-impl Day for Day08 {
-    type Input = String;
+#[derive(Debug)]
+pub struct Item {
+    pub observations: Vec<String>,
+    pub digits: Vec<String>,
+}
 
-    fn parse(_input: &str) -> IResult<&str, Self::Input> {
-        unimplemented!("parser")
+fn parse_line(input: &str) -> IResult<&str, Item> {
+    let (rest, (observations, _, digits)) = tuple((
+        separated_list0(
+            space1,
+            map(alpha1, |s: &str| s.chars().sorted().collect::<String>()),
+        ),
+        tag(" | "),
+        separated_list0(
+            space1,
+            map(alpha1, |s: &str| s.chars().sorted().collect::<String>()),
+        ),
+    ))(input)?;
+    Ok((
+        rest,
+        Item {
+            observations,
+            digits,
+        },
+    ))
+}
+
+impl Day for Day08 {
+    type Input = Vec<Item>;
+
+    fn parse(input: &str) -> IResult<&str, Self::Input> {
+        separated_list0(newline, parse_line)(input)
     }
 
-    type Output1 = String;
+    type Output1 = usize;
 
-    fn part_1(_input: &Self::Input) -> Self::Output1 {
-        unimplemented!("part_1")
+    fn part_1(input: &Self::Input) -> Self::Output1 {
+        println!("{:?}", input);
+        0
     }
 
     type Output2 = String;
